df312d88f02a43740d14cd695aa91ccdbdaaf838
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-09-02 16:04:19 -0300

    Update exercicio01.py
    range, append, for.

--- exercicio01.py
+++ exercicio01.py
@@ -5,8 +5,8 @@ for i in range(0, 5):
     lista.append(numeros)
     print(lista)
     lista.sort()
-    maior = lista[4:]
-    menor = lista[:1]
+    maior = lista[4]
+    menor = lista[1]
 print(f"Em ordem descrescente: {lista}")
 print(f"Maior valor: {maior}")
 print(f"Menor valor: {menor}")
