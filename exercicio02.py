2d23902ea34c1642e47d1c09c75c581165048a0c
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-09-02 16:08:04 -0300

    Delete exercicio02.py

--- exercicio02.py
+++
@@ -1,7 +0,0 @@
-lista = []
-for i in range(0, 5):
-    numeros = int(input("Digite um número: "))
-    if numeros not in lista:
-        lista.append(numeros)
-        lista.sort()
-        print(lista)
